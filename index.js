2eef40019052e301ed69888c32b969a7974a284a
BaiToshoo <toshetos04@gmail.com>
2024-08-21 19:45:41 +0300

    Implemented all game functionalities

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', solve);
 
 function solve() {
-
+    
     let player = 1;
     let player1Wins = 0;
     let player2Wins = 0;
-
+    
     const print = document.getElementById('print');
+    const result = document.getElementById('result');
+    const resetButton = document.getElementById('reset-btn');
+    
+    
     const b1 = document.getElementById('b1');
     const b2 = document.getElementById('b2');
     const b3 = document.getElementById('b3');
@@ -17,8 +21,7 @@ function solve() {
     const b8 = document.getElementById('b8');
     const b9 = document.getElementById('b9');
     
-    const resetButton = document.getElementById('reset')
-
+    
     b1.addEventListener('click', () => turn(b1))
     b2.addEventListener('click', () => turn(b2))
     b3.addEventListener('click', () => turn(b3))
@@ -28,8 +31,38 @@ function solve() {
     b7.addEventListener('click', () => turn(b7))
     b8.addEventListener('click', () => turn(b8))
     b9.addEventListener('click', () => turn(b9))
-    resetButton.addEventListener('click', reset)
-
+    resetButton.addEventListener('click', resetScore)
+    
+    function resetScore() {
+        player1Wins = 0
+        player2Wins = 0
+        result.textContent = `P1 - ${player1Wins} || P2 - ${player2Wins}`;
+    }
+    
+    function disableBoxes() {
+        b1.style.pointerEvents = 'none';
+        b2.style.pointerEvents = 'none';
+        b3.style.pointerEvents = 'none';
+        b4.style.pointerEvents = 'none';
+        b5.style.pointerEvents = 'none';
+        b6.style.pointerEvents = 'none';
+        b7.style.pointerEvents = 'none';
+        b8.style.pointerEvents = 'none';
+        b9.style.pointerEvents = 'none';
+    }
+    
+    function enableBoxes() {
+        b1.style.pointerEvents = 'auto';
+        b2.style.pointerEvents = 'auto';
+        b3.style.pointerEvents = 'auto';
+        b4.style.pointerEvents = 'auto';
+        b5.style.pointerEvents = 'auto';
+        b6.style.pointerEvents = 'auto';
+        b7.style.pointerEvents = 'auto';
+        b8.style.pointerEvents = 'auto';
+        b9.style.pointerEvents = 'auto';
+    }
+    
     function reset() {
         b1.textContent = '';
         b2.textContent = '';
@@ -42,8 +75,11 @@ function solve() {
         b9.textContent = '';
         player = 1;
         print.textContent = 'Player 1 turn(X)';
+        result.textContent = `P1 - ${player1Wins} || P2 - ${player2Wins}`;
+        enableBoxes()
     }
-
+    
+    
     function turn(b) {
         if (player === 1 && b.textContent === '') {
             b.textContent = 'X';
@@ -53,6 +89,47 @@ function solve() {
             b.textContent = 'O';
             print.textContent = 'Player 1 turn (X)';
             player = 1;
+        }
+        checkForWinner();
+    }
+    
+    function checkForWinner(){
+        const winningCombinations = [
+            [b1, b2, b3],
+            [b4, b5, b6],
+            [b7, b8, b9],
+            [b1, b4, b7],
+            [b2, b5, b8],
+            [b3, b6, b9],
+            [b1, b5, b9],
+            [b3, b5, b7]
+        ];
+        
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (a.textContent !== '' && a.textContent === b.textContent && a.textContent === c.textContent) {
+                print.textContent = `Player ${a.textContent === 'X' ? '1' : '2'} wins!`;
+                if (a.textContent === 'X') {
+                    player1Wins++;
+                } else {
+                    player2Wins++;
+                }
+                disableBoxes()
+                result.textContent = `P1 - ${player1Wins} || P2 - ${player2Wins}`;
+                setTimeout(reset, 2000);
+                
+                return;
+            } else {
+                checkForDraw();
+            }
+        }
+        
+        function checkForDraw() {
+            if (b1.textContent !== '' && b2.textContent !== '' && b3.textContent !== '' && b4.textContent !== '' && b5.textContent !== '' && b6.textContent !== '' && b7.textContent !== '' && b8.textContent !== '' && b9.textContent !== '') {
+                print.textContent = 'Draw!';
+                disableBoxes()
+                setTimeout(reset, 2000);
+            }
         }
     }
 }
